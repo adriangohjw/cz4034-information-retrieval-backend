7e257c202d42884eae4e44699e3faaa9bf2d9b41
adriangohjw <adriangohjw@gmail.com>
2021-03-29 01:11:04 +0800

    Add normalized_following_score

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,7 +18,7 @@ class Post < ApplicationRecord
 
   # score creator based on verified status, followers, following etc.
   def calculate_creator_score
-    normalized_followers_score * normalized_verified_score
+    normalized_followers_score * normalized_verified_score * normalized_following_score
   end
 
   # score post's impact and reach based on impressions, upvotes, reposts
@@ -28,6 +28,11 @@ class Post < ApplicationRecord
   def normalized_followers_score(histogram_bins: Post.followers_histogram[0])
     return 0 if histogram_bins.blank?
     Post.get_bin_position(value: self.followers, bins: histogram_bins).to_f / histogram_bins.size.to_f
+  end
+
+  def normalized_following_score(histogram_bins: Post.following_histogram[0])
+    return 0 if histogram_bins.blank?
+    (histogram_bins.size - Post.get_bin_position(value: self.following, bins: histogram_bins)).to_f / histogram_bins.size.to_f
   end
 
   def normalized_verified_score
@@ -44,6 +49,11 @@ class Post < ApplicationRecord
   def self.followers_histogram(followers_array: Post.all.pluck(:followers), histogram_size: 100)
     followers_array.reject! { |x| x.nil? }
     (bins, freqs) = followers_array.histogram(histogram_size)
+  end
+
+  def self.following_histogram(following_array: Post.all.pluck(:following), histogram_size: 100)
+    following_array.reject! { |x| x.nil? }
+    (bins, freqs) = following_array.histogram(histogram_size)
   end
 
   private
