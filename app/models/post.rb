66d06a2d0736d556d4692fa8598d46b7d0c937ec
adriangohjw <adriangohjw@gmail.com>
2021-03-28 15:42:06 +0800

    Add simple searchkick search_data structure

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+  searchkick
+
+  def search_data
+    {
+      body: body,
+      hashtags: hashtags,
+      creator_score: calculate_creator_score,
+      reach_score: calculate_reach_score,
+      posted_at: posted_at
+    }
+  end
+
+  # score creator based on verified status, followers, following etc.
+  def calculate_creator_score
+  end
+
+  # score post's impact and reach based on impressions, upvotes, reposts
+  def calculate_reach_score
+  end
+
 end
