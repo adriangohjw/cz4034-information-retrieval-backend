c3bf7ffacedd479bb5408ef9f73c8704a5e18744
adriangohjw <adriangohjw@gmail.com>
2021-03-29 00:54:55 +0800

    Add normalized_verified_score

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,7 +18,7 @@ class Post < ApplicationRecord
 
   # score creator based on verified status, followers, following etc.
   def calculate_creator_score
-    normalized_followers_score
+    normalized_followers_score * normalized_verified_score
   end
 
   # score post's impact and reach based on impressions, upvotes, reposts
@@ -26,8 +26,19 @@ class Post < ApplicationRecord
   end
 
   def normalized_followers_score(histogram_bins: Post.followers_histogram[0])
-    return nil if histogram_bins.blank?
+    return 0 if histogram_bins.blank?
     Post.get_bin_position(value: self.followers, bins: histogram_bins).to_f / histogram_bins.size.to_f
+  end
+
+  def normalized_verified_score
+    case self.verified
+    when true
+      return 1.0
+    when false
+      return 0.5
+    else
+      return 0.0
+    end
   end
 
   def self.followers_histogram(followers_array: Post.all.pluck(:followers), histogram_size: 100)
