2585b669a5ba887325e95d9fff20484ec735e1e5
adriangohjw <adriangohjw@gmail.com>
2021-03-29 01:58:04 +0800

    Add score migration to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_073636) do
+ActiveRecord::Schema.define(version: 2021_03_28_175748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2021_03_28_073636) do
     t.datetime "posted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "creator_score"
+    t.float "reach_score"
   end
 
 end
