fba2fdeeb50eab9a5a6466d6ab174bdc16efefc6
adriangohjw <adriangohjw@gmail.com>
2021-03-29 00:40:51 +0800

    Add noralized_followers_score

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,10 +18,21 @@ class Post < ApplicationRecord
 
   # score creator based on verified status, followers, following etc.
   def calculate_creator_score
+    normalized_followers_score
   end
 
   # score post's impact and reach based on impressions, upvotes, reposts
   def calculate_reach_score
+  end
+
+  def normalized_followers_score(histogram_bins: Post.followers_histogram[0])
+    return nil if histogram_bins.blank?
+    Post.get_bin_position(value: self.followers, bins: histogram_bins).to_f / histogram_bins.size.to_f
+  end
+
+  def self.followers_histogram(followers_array: Post.all.pluck(:followers), histogram_size: 100)
+    followers_array.reject! { |x| x.nil? }
+    (bins, freqs) = followers_array.histogram(histogram_size)
   end
 
   private
@@ -29,6 +40,17 @@ class Post < ApplicationRecord
   def self.remove_words_from_text(text:, words:) 
     words&.each { |word| text.slice!(word) }
     return text;
+  end
+
+  def self.get_bin_position(value:, bins:)
+    return nil if value.blank?
+    
+    counter = 0
+    bins.each do |bin|
+      break if value <= bin
+      counter = counter + 1
+    end
+    return counter
   end
 
 end
