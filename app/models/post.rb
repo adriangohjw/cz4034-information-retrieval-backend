211c8d2df3263038db6773681a6c907d5395b1e9
adriangohjw <adriangohjw@gmail.com>
2021-03-28 22:35:51 +0800

    Clean Post.body before reindexing

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,16 @@ class Post < ApplicationRecord
 
   def search_data
     {
-      body: body,
+      body: preprocessed_body,
       hashtags: hashtags,
       creator_score: calculate_creator_score,
       reach_score: calculate_reach_score,
       posted_at: posted_at
     }
+  end
+
+  def preprocessed_body
+    Post.remove_words_from_text(text: body, words: hashtags)
   end
 
   # score creator based on verified status, followers, following etc.
@@ -18,6 +22,13 @@ class Post < ApplicationRecord
 
   # score post's impact and reach based on impressions, upvotes, reposts
   def calculate_reach_score
+  end
+
+  private
+
+  def self.remove_words_from_text(text:, words:) 
+    words&.each { |word| text.slice!(word) }
+    return text;
   end
 
 end
