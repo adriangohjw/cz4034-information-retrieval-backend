c251ac98cbec73c7bdfbfa8c4b814ee7aace3fbe
Puay Hiang Goh <Goh.PuayHiang@outlook.com>
2021-04-10 17:40:50 +0800

    Update api.js

--- api.js
+++ api.js
@@ -12,7 +12,7 @@ const client = new Client({ node: 'http://localhost:9200'}) //, requestTimeout: 
 const querystring = require('querystring');
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
@@ -208,6 +208,104 @@ function elasticSuggest(search_term, hashtag = [], query_from = 0, query_size = 
  
  }
 
-app.listen(port, () => {
+
+
+ app.get('/distribution', (req, res) => {
+  // let from = req.query.from;  
+  // let size = req.query.size;  
+
+  let search_term = req.query.search_term;
+  let fieldname = req.query.field;
+
+  if(typeof search_term != 'string'){
+    res.end("Invalid");
+    return;
+  }
+
+  let hashtags = req.query.hashtags;
+  if(typeof hashtags == 'string'){
+    hashtags = hashtags.replace(/[`~!@#$%^&*()_|+\-=?;:'".<>\{\}\[\]\\\/]/gi,"")
+    hashtags = hashtags.split(",");
+  }
+  // console.log(
+    
+    elasticDistribution(search_term, hashtags, fieldname)
+    .then((result, err) => {
+        if (err){ 
+          console.log(err)
+          res.end(JSON.stringify({distribution:[]}));
+        }else{
+          res.end(JSON.stringify({distribution:result.body.aggregations.result_percentile.values}));
+          
+        }
+        // console.log(err)
+        // console.log(result)
+      }).catch((err)=>
+        {
+          console.log(err);
+
+        })
+})
+
+function elasticDistribution(search_term, hashtag = [], fieldName = "upvotes"){
+
+  var querySearch = {
+      "fuzzy": {
+          "body": {
+              "value": search_term,
+              "fuzziness": "AUTO",
+              "max_expansions": 50,
+              "prefix_length": 0,
+              "transpositions": true,
+              "rewrite": "constant_score"
+          }
+      }
+  };
+  
+  if(hashtag.length > 0){
+    querySearch = {
+      bool:{
+        should: querySearch,
+        must: {
+          "terms": {
+            "hashtags": hashtag,
+            "boost": 1
+          }
+        }
+      }
+    }
+  }
+
+   return client.search({
+      index: 'parler_posts',
+      body: {
+          query:  querySearch,
+          aggs: {
+            result_percentile: {
+              percentiles: {
+                field: fieldName,
+                 percents: [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100 ],
+                 "hdr": {                                  
+                  "number_of_significant_value_digits": 5
+                }
+              }
+            }
+          }
+          
+      }
+    }
+    // , (err, result) => {
+    //   if (err){ 
+    //     console.log(err)
+    //     return [];
+    //   }else{
+    //     console.log(result)
+    //     return result.body.hits.hits;
+    //   }
+    // }
+    )
+}
+
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
